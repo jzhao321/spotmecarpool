32c783564a021cd101ee14797fd628721299fb94
James Zhao <james.zhao@sjsu.edu>
2018-10-11 03:43:57 -0700

    Made home test page

---
+++ app.js
@@ -0,0 +1,113 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var Sequelize = require("sequelize");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+var seq = new Sequelize("spotme_garages", "root", "spot123", {
+    host: "35.227.173.37",
+    dialect: "mysql",
+    operatorsAliases:false,
+
+    pool:{
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+const garage = seq.define("garage", {
+    name: {
+        type: Sequelize.STRING
+    },
+    current:{
+        type: Sequelize.INTEGER
+    },
+    max:{
+        type: Sequelize.INTEGER
+    }
+});
+
+app.get("/", function(req,res){
+    res.send("Test home page");
+});
+
+app.get("/allData", function(req,res){
+    garage.findAll().then(function(result){
+        res.send(JSON.stringify(result));
+    });
+});
+
+app.get("/test", function(req,res){
+    res.send("Test123");
+});
+
+app.get("/Ddata", function(req,res){
+    garage.create({
+        name: "SJSUSouth",
+        current: 300,
+        max:500
+        
+    }).then(function(result){
+        res.send(JSON.stringify(result));
+    });
+});
+
+
+app.get("/create", function(req,res){
+    garage.sync({force:true}).then(function(result){
+        console.log(result);
+    });
+});
+
+app.post("/garage", function(req, res){
+    garage.find({
+        where:{
+            name: req.body.name
+        },
+        attributes:{
+            exclude: ["createdAt","updatedAt"]
+        }
+
+    }).then(function(result){
+        res.send(JSON.stringify(result));
+    });
+});
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
