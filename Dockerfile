7f60398b1dfda422b9e690231f233a374374a180
James Zhao <zhaojyl935@gmail.com>
2019-01-15 22:31:58 -0800

    Cleaned up Docker image, moved building to local machine

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:10.9.0-alpine
 WORKDIR /code
 COPY package*.json ./
-RUN npm install
+RUN npm install --only=prod
 COPY . .
 ENV DATABASE_URL postgres://uwubiozhpribth:d8497db54422048576bf2b7a695087d05175648adaa444cfe5af368cbd634c34@ec2-54-163-246-159.compute-1.amazonaws.com:5432/df0s02iv6v9564
 EXPOSE 3000
--- src/app.js
+++ src/app.js
@@ -4,11 +4,21 @@ import path from "path";
 import pg from "pg";
 import cors from "cors";
 
+
 //Set PostGres defualt options
 pg.defaults.ssl = true;
 
 //Declare application variable for express
 var app = express();
+
+try{
+    console.log(require.resolve("morgan"));
+    console.log("Development Mode Activated");
+    app.use(require("morgan")("dev"));
+}
+catch(e){
+}
+
 
 //Enables Cross Origin Requests (CORS) support
 app.use(cors());
@@ -37,19 +47,19 @@ app.get("/", (req, res) => {
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.status(404);
-  res.send("404 File Not Found");
+    res.status(404);
+    res.send("404 File Not Found");
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
